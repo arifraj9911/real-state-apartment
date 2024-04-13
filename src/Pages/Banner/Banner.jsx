b5981ae84eb2e55e6dba51cc8656982fb888ca33
arifraj199 <karif9514@gmail.com>
2024-04-13 13:53:09 +0600

    banner slider with swipper js slider implement

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,18 +1,20 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-import roomImg from '../../assets/Images/room-1.jpg';
+import { Swiper, SwiperSlide } from "swiper/react";
+import roomImg from "../../assets/Images/room-1.jpg";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+// import './Banner.css'
 
 const Banner = () => {
   return (
     <Swiper
-    className='rounded-xl my-8 z-0'
+      className="rounded-xl z-0"
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
@@ -21,31 +23,73 @@ const Banner = () => {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
     >
-      {/* <SwiperSlide style={{
-        backgroundImage:`linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${roomImg})`,
-        backgroundPosition:'center',
-        backgroundSize:'cover',
-        height:'130vh',
-        backgroundRepeat:'no-repeat'
-
-      }}></SwiperSlide> */}
-      <div className="bg-[url('https://i.ibb.co/JCmp3p9/room-1.jpg')]">
-      <SwiperSlide></SwiperSlide>
-      </div>
-      <div>
-      <SwiperSlide><img src={roomImg} alt="" /></SwiperSlide>
-      </div>
-      <div>
-      <SwiperSlide><img src={roomImg} alt="" /></SwiperSlide>
-      </div>
-      {/* <SwiperSlide><img src={roomImg} alt="" /></SwiperSlide>
-      <SwiperSlide><img src={roomImg} alt="" /></SwiperSlide> */}
-      {/* <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide> */}
-      ...
+      <SwiperSlide>
+        <div
+          className="hero min-h-96 lg:min-h-screen"
+          style={{
+            backgroundImage: `url(${roomImg})`,
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content w-4/5 flex justify-start  text-left text-white">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Luxurious Palace</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Know More</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className="hero min-h-96 lg:min-h-screen"
+          style={{
+            backgroundImage:
+              "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content w-4/5 flex justify-start  text-left text-white">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div
+          className="hero  min-h-96 lg:min-h-screen"
+          style={{
+            backgroundImage:
+              "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
+          }}
+        >
+          <div className="hero-overlay bg-opacity-60"></div>
+          <div className="hero-content w-4/5 flex justify-start  text-left text-white">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-5xl font-bold">Luxurious Palace </h1>
+              <p className="mb-5">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button className="btn btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 };
