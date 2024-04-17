16f98b5b7669f4ff37d3e974bf85b11c9c2e729e
arifraj199 <karif9514@gmail.com>
2024-04-17 14:56:37 +0600

    environment variable error issue solved

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_apiKey,
-  authDomain: import.meta.env.VITE_API_authDomain,
-  projectId: import.meta.env.VITE_API_projectId,
-  storageBucket: import.meta.env.VITE_API_storageBucket,
-  messagingSenderId: import.meta.env.VITE_API_messagingSenderId,
-  appId: import.meta.env.VITE_API_appId,
+  apiKey: import.meta.env.VITE_API_APIKEY,
+  authDomain: import.meta.env.VITE_API_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_API_PROJECTID,
+  storageBucket: import.meta.env.VITE_API_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_API_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_API_APPID,
 };
 
 // Initialize Firebase
